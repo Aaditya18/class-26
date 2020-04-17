dd3ff06b8cdf88985a776497e11f6c3e13fe3fab
Aaditya18 <aadikumar1811@gmail.com>
2020-04-17 19:58:44 +0530

    hello

--- sketch.js
+++ sketch.js
@@ -15,10 +15,11 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+//added ground
     ground = new Ground(600,height,1200,20);
+   // added a platform
     platform = new Ground(150, 305, 300, 170);
-
+//add boxes
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
